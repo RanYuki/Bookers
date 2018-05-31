c2943c73c3ca43ba167eee9cb674821e466b122f
結城蘭 <yukiran@r-the-computar.local>
2018-05-31 19:31:33 +0900

    made some revisions. please kindly check it

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,8 @@
 class PostsController < ApplicationController
+
   def index
     @posts = Post.all
-     @post = Post.new(post_params)
+     @post = Post.new
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-	resources :posts
-    get 'post/top' => 'root#top'
+
+  resources :posts
+  root 'root#top'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
